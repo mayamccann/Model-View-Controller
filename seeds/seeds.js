328ae8ebc28f0945ed2cccb6b0363ec7b976e552
Maya McCann <mccannmaya@gmail.com>
2023-11-15 14:08:16 -0500

    const seedDatabase and seedDatabase end code

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,3 +1,9 @@
 const sequelize = require('../config/connection');
 const { User } = require('../models');
 
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true});
+
+};
+
+seedDatabase();
