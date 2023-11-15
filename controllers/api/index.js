26c9504b38fda705fdf33ccad2818cc0ebc5cf7d
Maya McCann <mccannmaya@gmail.com>
2023-11-15 13:17:14 -0500

    router code for the api route with the homeRoutes js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,3 +2,7 @@ const router = require('').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes)
+
