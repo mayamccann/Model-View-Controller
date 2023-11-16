2b088c54c2a928c2aacb220543a04ce1764fa4d5
Maya McCann <mccannmaya@gmail.com>
2023-11-16 12:40:20 -0500

    commentRoutes.js const for index.js for api for controllers

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,6 @@
 const router = require().Router();
 const userRoutes = require('./userRoutes');
+const commentRoutes = require('./commentRoutes');
 const projectRoutes = require('./projectRoutes');
 
 router.use('/users', userRoutes);
