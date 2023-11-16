3e3aa0be8a4d72808cdd239f5cadd59e86c3bb9d
Maya McCann <mccannmaya@gmail.com>
2023-11-16 13:11:13 -0500

    router.get try for homeRoutes.js for controllers

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,18 @@ const router = require('express').Router();
 const { Project, User} = require();
 const withAuth = require();
 
+router.get('/', async (req, res) => {
+    try {
+        const projectData = await Project.findAll ({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
 
 
 
+
+module.exports = router;
