627b754d8f08be116e8fc197b800072b5108eeee
Maya McCann <mccannmaya@gmail.com>
2023-11-16 13:16:16 -0500

    index.js router.use code for homeRoutes and apiRoutes

--- controllers/index.js
+++ controllers/index.js
@@ -5,5 +5,7 @@ const homeRoutes = require('./homeRoutes');
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 module.exports = router;
