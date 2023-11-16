d50dbbc8fdf6a085455e104e4cb01674f778a0f3
Maya McCann <mccannmaya@gmail.com>
2023-11-16 12:22:38 -0500

    id and sequelize for User.init for user.js for models

--- models/user.js
+++ models/user.js
@@ -3,7 +3,25 @@ const sequelize = require('../config/connection');
 
 
 User.init( 
+{
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+},
 
+
+{
+
+sequelize,
+timestamps: false,
+freezeTableName: true,
+underscored: true,
+modelName: 'user',
+
+}
 
 );
 
