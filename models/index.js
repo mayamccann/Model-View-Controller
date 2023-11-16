43bdecb4c6513a0db55af0b77f18c3b41d7b2354
Maya McCann <mccannmaya@gmail.com>
2023-11-16 12:00:51 -0500

    const user, post and comment for models

--- models/index.js
+++ models/index.js
@@ -1,4 +1,6 @@
 const User = require('./user');
+const Post = require('./blogpost');
+const Comment = require('./comment');
 
 
-module.exports = { User };
+module.exports = { User, Post, Comment };
