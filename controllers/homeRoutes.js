29a84cb74b45f72aae46e3e55da6cf83a1fee5c6
Maya McCann <mccannmaya@gmail.com>
2023-11-16 13:13:35 -0500

    const project and res.render and catch err for homeRoutes.js for controllers

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,7 +13,18 @@ router.get('/', async (req, res) => {
             ],
         });
 
+        // const projects
+        const projects = projectData.map((project) => project.get({ plain: true }));
 
+      // for next res.render homepage
+        res.render('homepage', { 
+          projects, 
+          logged_in: req.session.logged_in 
+        });
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    });
 
 
 module.exports = router;
