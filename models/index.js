ebd7ebdac376eb74e04d185582103aa78995acc2
Maya McCann <mccannmaya@gmail.com>
2023-11-16 22:21:42 -0500

    Comments for remaining code, User.hasMany foreignKey, onDelete for index.js for models

--- models/index.js
+++ models/index.js
@@ -2,5 +2,15 @@ const User = require('./user');
 const Post = require('./blogpost');
 const Comment = require('./comment');
 
+// Defining and connecting files of models
+
+User.hasMany(Post) {
+    foreignKey:
+    onDelete: 
+}
+
+
+
+
 
 module.exports = { User, Post, Comment };
